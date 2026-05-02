6711acb7b380d6d0e4b173e7c033cdbe5e84b9b0
Your Name <itismepranish@gmail.com>
2026-05-02 19:12:49 +0545

    feat(UserController): Added User Controller with /me api

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+using System.Security.Claims;
+using backend.Data.DTO.Response;
+using backend.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class UserController(
+    UserManager<User> userManager
+    ) : ControllerBase
+{
+    [HttpGet("me")]
+    public async Task<ActionResult<UserAuthResponse>> Me()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId is null) return Unauthorized();
+
+        var user= await userManager.FindByIdAsync(userId);
+        if(user is null) return NotFound("User does not exist");
+
+        var roles = await userManager.GetRolesAsync(user);
+
+        return Ok(new UserAuthResponse
+        {
+            Id = user.Id,
+            Name = user.FirstName + " " + user.LastName,
+            Email = user.Email ?? "",
+            PhoneNumber = user.PhoneNumber ?? "",
+            Roles = roles
+        });
+    }
+}
---
+++ backend/Data/DTO/Response/UserAuthResponse.cs
@@ -0,0 +1,11 @@
+namespace backend.Data.DTO.Response;
+
+public class UserAuthResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public IList<string> Roles { get; set; } = [];
+
+}
