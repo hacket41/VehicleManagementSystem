83d3b0a9045d2ae41dde0546639674e7c3df25a9
Your Name <itismepranish@gmail.com>
2026-05-09 17:38:11 +0545

    feat(docker): Set up docker

--- backend/Program.cs
+++ backend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:3001")
+        policy.WithOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:3001")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
@@ -139,3 +139,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+// Aws Password: Postgres123
